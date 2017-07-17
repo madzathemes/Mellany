a67a8896a50371ad857e937aed3e0e52394a372a
madzathemes <madza@madzathemes.com>
2017-07-17 04:07:35 +0300

    fix menu

--- inc/functions/functions-widget.php
+++ inc/functions/functions-widget.php
@@ -152,6 +152,21 @@ if(function_exists( 'is_woocommerce' ) ) {
 	) );
 
 
+	/* --------------------------------------------------------------------------------------- Mobile Menu Widgets */
+
+		register_sidebar( array(
+			'name' => esc_html__( 'Mobile Menu Widgets', 'mellany'),
+			'id' => 'mobile-menu-widget-area',
+			'description' => esc_html__( 'The Mobile Menu Widget area' , 'mellany'),
+			'before_widget' => '<div class="widget">',
+			'after_widget' => '<div class="clear"></div></div>',
+					'before_title' => '<h2 class="heading"><span>',
+					'after_title' => '</span></h4>',
+		) );
+
+
+
+
 }
 
 add_action( 'widgets_init', 'mellany_sidebar_widget_init' );
